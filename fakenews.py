782cd9faf30b55c6139dcf4a42f03731613f7d9e
Salim <salim.nevermind@gmail.com>
2018-03-15 02:24:55 +0100

    TFIDF Implemented

--- fakenews.py
+++ fakenews.py
@@ -1,19 +1,18 @@
 import feedparser
+import math
 import re   
 from pymongo import MongoClient
 import html
 import nltk
 import json
 from nltk.corpus import stopwords # Import the stop word list
-
+from textblob import TextBlob as tb
 
 
 def connecToDatabse():
     client = MongoClient('mongodb://ecam_algo:ecamalgo2018@ds012168.mlab.com:12168/fakenews')
     return client
     
-
-
 
 def setNews(client):
     db = client.fakenews
@@ -63,15 +62,34 @@ def getBadWords():
     badwords = json.load(open('badwords.json'))
     return badwords["badwords"]
 
-def detectFakeNews(meaningfulWords, badwords):
-    print(meaningfulWords['5aa108e3b3f7112e30054ecd'])
+def detectBadWords(meaningfulWords, badwords):
+   # print(meaningfulWords['5aa108e3b3f7112e30054ecd'])
     for k, v in meaningfulWords.items():
         numberOfMeaningfulWords = len(v)
         numberOfBadWords = len([w for w in v if w in badwords])
-        print(numberOfBadWords, numberOfMeaningfulWords, k)
 
+def getMostImportantWords(meaningfulWords):
+    for k, v in meaningfulWords.items():
+        #print("Top words in document {}".format(k))
+        result = tb(" ".join(v))
+        scores = {word: tfidf(word, result, meaningfulWords) for word in result.words}
+        sorted_words = sorted(scores.items(), key=lambda x: x[1], reverse=True)
+        if (k == '5aa108e3b3f7112e30054ecd'): 
+            for word, score in sorted_words[:10]:
+                print("Word: {}, TF-IDF: {}".format(word, round(score, 5)))
 
-        
+def tf(word, meaningfulWordsValue):
+    return meaningfulWordsValue.words.count(word) / len(meaningfulWordsValue)
+
+def n_containing(word, meaningfulWords):
+    return sum(1 for v in meaningfulWords if word in v) 
+
+def idf(word, meaningfulWords):
+    return math.log(len(meaningfulWords) / (1 + n_containing(word, meaningfulWords)))
+
+def tfidf(word, meaningfulWordsValue, meaningfulWords):
+    return tf(word, meaningfulWordsValue) * idf(word, meaningfulWords)
+
         
 
 # ObjectId('5aa108e2b3f7112e30054eb5')
@@ -81,5 +99,6 @@ if __name__ == '__main__':
     client = connecToDatabse()
     result = getMeaningfulWords(client)
     badWords = getBadWords()
-    numberOfBadWords, numberOfMeaningfulWords = detectFakeNews(result, badWords)
-    print(numberOfBadWords, numberOfMeaningfulWords)
+    detectBadWords(result, badWords)
+    getMostImportantWords(result)
+
